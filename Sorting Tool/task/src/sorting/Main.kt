36691b285f30346dd6521a323107c8db2d26040d
Anatoliy <anartius@gmail.com>
2021-09-12 20:28:26 +0200

    Finish of stage 1

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -1,5 +1,17 @@
 package sorting
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val inputData = mutableListOf<Int>()
+    while (scanner.hasNextInt()) {
+        inputData.add(scanner.nextInt())
+    }
+    val max = inputData.maxOf { it }
+    val quantity = inputData.count { it == max }
+    println("""
+        Total numbers: ${inputData.size}
+        The greatest number: $max ($quantity time(s)).
+    """.trimIndent())
 }
