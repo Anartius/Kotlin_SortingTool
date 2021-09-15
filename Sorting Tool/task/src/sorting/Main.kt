c8ab160417637dec7a3a7b49699aef6fa89735c3
Anatoliy <anartius@gmail.com>
2021-09-15 22:17:58 +0200

    Finish of stage 2

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -2,16 +2,63 @@ package sorting
 
 import java.util.*
 
-fun main() {
-    val scanner = Scanner(System.`in`)
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    when(args[args.lastIndex]) {
+        "long" -> numbers()
+        "line" -> line()
+        else -> words()
+    }
+}
+
+fun numbers() {
     val inputData = mutableListOf<Int>()
+
     while (scanner.hasNextInt()) {
         inputData.add(scanner.nextInt())
     }
     val max = inputData.maxOf { it }
     val quantity = inputData.count { it == max }
     println("""
-        Total numbers: ${inputData.size}
-        The greatest number: $max ($quantity time(s)).
+        Total numbers: ${inputData.size}.
+        The greatest number: $max ($quantity time(s), ${percent(inputData.size, quantity)}%).
     """.trimIndent())
+}
+
+fun line() {
+    val inputData = mutableListOf<String>()
+
+    while (scanner.hasNextLine()) {
+        inputData.add(scanner.nextLine())
+    }
+    val max = inputData.maxOf { it.length }
+    val maxLine = inputData.first { it.length == max }
+    val quantity = inputData.count { it.length == max }
+    println("""
+        Total lines: ${inputData.size}.
+        The longest line: 
+        $maxLine
+        ($quantity time(s), ${percent(inputData.size, quantity)}%).
+    """.trimIndent())
+}
+
+fun words() {
+
+    val inputData = mutableListOf<String>()
+
+    while (scanner.hasNext()) {
+        inputData.add(scanner.next())
+    }
+    val max = inputData.maxOf { it.length }
+    val maxLine = inputData.first { it.length == max }
+    val quantity = inputData.count { it.length == max }
+    println("""
+        Total words: ${inputData.size}.
+        The longest word: $maxLine ($quantity time(s), ${percent(inputData.size, quantity)}%).
+    """.trimIndent())
+}
+
+fun percent(quantity: Int, total: Int) : Int {
+    return total * 100 / quantity
 }
